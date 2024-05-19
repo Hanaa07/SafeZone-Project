f9ccdb1069eac005f3da6ee83de703fa86d7d0b2
yousraeb <yusraeb123@gmail.com>
2024-05-19 02:07:16 +0100

    02.07

--- src/screens/googlemaps/index.js
+++ src/screens/googlemaps/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import markerIcon from './markerIcon.png';
-import MapView, { Marker, mapKit } from 'react-native-maps';
+import MapView, { Marker, Polyline, mapKit } from 'react-native-maps';
 const fetch = require('node-fetch');
 
 const LATITUDE = 31.63416;
@@ -11,15 +11,15 @@ const LONGITUDE_DELTA = 0.1;
 
 let id = 0;
 const GoogleMapsScreen = () => {
-  // region
   const [region, setRegion] = useState({
     latitude: LATITUDE,
     longitude: LONGITUDE,
     latitudeDelta: LATITUDE_DELTA,
     longitudeDelta: LONGITUDE_DELTA,
   });
-  // markers
+ 
   const [markers, setMarkers] = useState([]);
+  const [polylineCoords, setPolylineCoords] = useState([]);
 
   // setting markers
   const onMapPress = (e) => {
@@ -28,7 +28,24 @@ const GoogleMapsScreen = () => {
       key: id++,
     };
     setMarkers([...markers, newMarker]);
+    setPolylineCoords([...polylineCoords, e.nativeEvent.coordinate]);
     console.log('New Marker Added:', newMarker);
+  };
+  // drag marker 
+  const onMarkerDragEnd = (e, markerKey) => {
+    const newCoordinate = e.nativeEvent.coordinate;
+    setMarkers((prevMarkers) =>
+      prevMarkers.map((marker) =>
+        marker.key === markerKey
+          ? { ...marker, coordinate: newCoordinate }
+          : marker
+      )
+    );
+    setPolylineCoords((prevCoords) =>
+      prevCoords.map((coord, index) =>
+        markers[index]?.key === markerKey ? newCoordinate : coord
+      )
+    );
   };
 
   // save coordinates
@@ -43,6 +60,13 @@ const GoogleMapsScreen = () => {
         console.log('Marker Data:', data);
         return data;
       });
+      
+         // Close the polyline by adding the first marker's coordinate at the end
+         const closedPolylineCoords = [
+          ...polylineCoords,
+          polylineCoords[0],
+        ];
+        setPolylineCoords(closedPolylineCoords);
 
       // Post marker data to your database
       try {
@@ -82,10 +106,12 @@ const GoogleMapsScreen = () => {
           <Marker
             key={marker.key}
             coordinate={marker.coordinate}
-            image={markerIcon}
-            style={styles.markerIcon}
+            pinColor='red'
+            draggable
+            onDragEnd={(e)=> onMarkerDragEnd(e,marker.key)}
           />
         ))}
+        <Polyline coordinates={polylineCoords} strokeColor="#FF0000" strokeWidth={2} />
       </MapView>
       <View style={styles.buttonContainer}>
         <Button title="Save" onPress={saveCoord} style={styles.button} />
@@ -94,7 +120,7 @@ const GoogleMapsScreen = () => {
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   buttonContainer: {
     marginBottom: 25,
   },
@@ -105,6 +131,6 @@ const styles = {
     color: '#000',
     fontWeight: 'bold',
   },
-};
+});
 
 export default GoogleMapsScreen;
