5c57f3f9da7113aea3bfc6e6cee517da4e2edb01
ST3ERLING <ST3RLING@email.com>
2024-05-24 08:52:32 +0100

    Update backend

--- backend/models/Zone.js
+++ backend/models/Zone.js
@@ -6,7 +6,11 @@ var ZoneSchema = new Schema({
     coordinates: [{
         type: Schema.Types.ObjectId, // Reference to the Coordonnees model
         ref: 'Coordinates'
-    }]
+    }],
+    typeZone: {
+        type: String,
+        enum: ["erruption", "flood", "fire", "hazard", "attackers", "tornado"]
+    }
 });
 
 var Zone = mongoose.model("Zone", ZoneSchema, "Zone");
